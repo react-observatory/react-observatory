1a5b8d00cb536b7f8599e48ce45014b9069b11f7
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-03-21 10:22:10 +0100

    Added a test for injectReducer.

---
+++ src/__tests__/injectReducer.spec.js
@@ -0,0 +1,60 @@
+/**
+ * Test injectors
+ */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+import identity from 'lodash/identity';
+
+import configureStore from 'redux-mock-store';
+import injectReducer from '../injectReducer';
+import * as reducerInjectors from '../reducerInjectors';
+
+// Fixtures
+const Component = () => null;
+
+const reducer = identity;
+
+const mockStore = configureStore();
+
+describe('injectReducer decorator', () => {
+  let store;
+  let injectors;
+  let ComponentWithReducer;
+
+  beforeAll(() => {
+    reducerInjectors.default = jest.fn().mockImplementation(() => injectors);
+  });
+
+  beforeEach(() => {
+    store = mockStore();
+    injectors = {
+      injectReducer: jest.fn()
+    };
+    ComponentWithReducer = injectReducer('test', reducer)(Component);
+    reducerInjectors.default.mockClear();
+  });
+
+  it('should inject a given reducer', () => {
+    shallow(<ComponentWithReducer />, { context: { store } });
+
+    expect(injectors.injectReducer).toHaveBeenCalledTimes(1);
+    expect(injectors.injectReducer).toHaveBeenCalledWith('test', reducer);
+  });
+
+  it('should set a correct display name', () => {
+    expect(ComponentWithReducer.displayName).toBe('withReducer(Component)');
+    expect(injectReducer('test', reducer)(() => null).displayName).toBe(
+      'withReducer(Component)'
+    );
+  });
+
+  it('should propagate props', () => {
+    const props = { testProp: 'test' };
+    const renderedComponent = shallow(<ComponentWithReducer {...props} />, {
+      context: { store }
+    });
+
+    expect(renderedComponent.prop('testProp')).toBe('test');
+  });
+});
