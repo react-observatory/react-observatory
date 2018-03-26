47d38e70d32a5e0aa7d32c53bfbb78c816a7f1df
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-03-26 14:42:07 +0200

    Add an example to `applyAsyncEpics`

--- src/applyAsyncEpics.js
+++ src/applyAsyncEpics.js
@@ -3,6 +3,29 @@ import { EPICS_STREAM_SYMBOL, INJECTED_EPICS_PROPERTY } from './constants';
 /**
  * Creates a store enhancer that enables dynamic injection of epics.
  *
+ * ```js
+ * import { createStore, compose, applyMiddleware } from 'redux'
+ * import { applyAsyncEpics } from 'react-observatory'
+ * import { createEpicMiddleware } from 'redux-observable'
+ * import reducers from './reducers'
+ * import { rootEpic, epic$ } from './epics'
+ *
+ * const epicMiddleware = createEpicMiddleware(rootEpic)
+ *
+ * export default function configureStore(initialState = {}) {
+ *  const store = createStore(
+ *    reducers,
+ *      initialState,
+ *      compose(
+ *       applyAsyncEpics(epic$),
+ *       applyMiddleware(epicMiddleware)
+ *     )
+ *   )
+ *
+ *   return store
+ * }
+ *  ```
+ *
  * @param epics$ {@class BehaviorSubject<T>} A stream of epics.
  * @returns {function} A store enhancer applying the middleware.
  */
